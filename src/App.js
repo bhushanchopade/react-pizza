f4016020544ebfec5beb713669f671f637ccb83a
bhushanchopade <bhushan.chopade7596@gmail.com>
2022-04-18 20:11:17 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+
+    <Router>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      
+        <Switch>
+            <Route exact path='/' component={Home}></Route>
+            <Route path='/about' component={About}></Route>
+        </Switch>
+    </Router>
+
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/pages/Home.js
@@ -0,0 +1,6 @@
+const Home = () => {
+    return <h1>welcome to home page</h1>
+}
+
+export default Home;
+
